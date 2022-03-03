8acab32f0020a0bbfec1e7a345ab493a7b2526e9
Valentina D'Orazio <valentina_dorazio@virgilio.it>
2022-03-03 14:27:25 +0100

    Revert "a"
    This reverts commit c16edfa0a05df587dc9628e23072aee8084c053d.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { Valendina } from './Valendina/Valendina';
 
 function App() {
-  const pippo = 'ha funzionatoooooo';
+  const pippo = 'ha funzionatoooooooooooo';
   const evviva = 'non so come ma pare che ha capito';
   return (
     <div className="App">
