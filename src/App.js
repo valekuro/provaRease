a1a5f6052dae959de8c13c72a98947a9ca560604
valekuro <valentina_dorazio@virgilio.it>
2022-03-03 16:14:54 +0100

    NOH

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React BOOOOOH
         </a>
       </header>
     </div>
