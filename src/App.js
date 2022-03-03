a040926190aa880079d5d2bd54925eee436727a4
valekuro <valentina_dorazio@virgilio.it>
2022-03-03 12:00:26 +0100

    aaaa

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 
 function App() {
   const pippo = 'ha funzionatoooooo';
+  const boh = ()=>{
+    console.log('ciao')
+  }
   const evviva = 'sto commit funziona';
   return (
     <div className="App">
       <header className="App-header">
-      <p>ciao</p>
+      <p>{pippo}</p>
       </header>
     </div>
   );
