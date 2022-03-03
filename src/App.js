5afb4f7269761d534f828ccc3b1536546b1e96b1
Valentina D'Orazio <valentina_dorazio@virgilio.it>
2022-03-03 14:27:25 +0100

    Revert "commituno"
    This reverts commit 5e09f55fb794bb68a95a140f448469a026de4761.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const pippo = 'ha funzionatoooooooooooo';
+  const pippo = 'ciao';
   const oddioSanto = 'sto cazzo di rebase';
   return (
     <div className="App">
