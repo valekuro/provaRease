911989cd9452d9650679b5c7073e9eed8d9e0e28
valekuro <valentina_dorazio@virgilio.it>
2022-03-04 10:26:32 +0100

    conflitti risolti

--- src/Valendina/Valendina.js
+++ src/Valendina/Valendina.js
@@ -3,7 +3,7 @@ export const Valendina=()=>{
         <div>
             <span>un'idea gentilmente offerta da Alice</span>
             <span>pippo, pluto e paperino vanno a spasso per Piazza Garibaldi con Spritz</span>
-            <span>cerca il form approvazione responsabile</span>
+            <span>dhjkdh djk fhd h fadfh dfh ad ad</span>
         </div>
     )
 }
--- src/Valendina/valentinaForm.js
+++ src/Valendina/valentinaForm.js
@@ -1,7 +1,2 @@
 console.log("valendina");
-//weweeeeeeeee fjkrgionerklgniodrklgherklbgker
-//nfkjerssgnkerlgnkler
-<<<<<<< HEAD
-=======
-//ORCAMADO VALENDINAAAAAAAAAAAAAAAAA
->>>>>>> 6180a3fc99ad8dc81e5799e9126c520d0878887f
+
