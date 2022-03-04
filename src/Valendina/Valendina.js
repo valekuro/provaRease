efd5777e44f73106a381bf39527ffeabc3f67769
valekuro <valentina_dorazio@virgilio.it>
2022-03-04 10:21:45 +0100

    mah forse funziona

--- src/Valendina/Valendina.js
+++ src/Valendina/Valendina.js
@@ -3,6 +3,7 @@ export const Valendina=()=>{
         <div>
             <span>un'idea gentilmente offerta da Alice</span>
             <span>pippo, pluto e paperino vanno a spasso per Piazza Garibaldi con Spritz</span>
+            <span>cerca il form approvazione responsabile</span>
         </div>
     )
 }
